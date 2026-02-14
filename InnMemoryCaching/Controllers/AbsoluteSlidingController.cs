34f3d7b51bfbf757565a285b572e832e80ce78dc
ömer yaşar <omeryasar745@gmail.com>
2026-02-15 00:30:59 +0300

    Add project files.

---
+++ InnMemoryCaching/Controllers/AbsoluteSlidingController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InnMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AbsoluteSlidingController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public AbsoluteSlidingController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            var cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            };
+
+            _memoryCache.Set<DateTime>("date", DateTime.Now, cacheEntryOptions);
+        }
+
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+
+    }
+}
---
+++ InnMemoryCaching/Controllers/ValueController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InnMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValueController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("set/{name}")]
+        public void Set(string name)
+        {
+            _memoryCache.Set("key", "name", TimeSpan.FromSeconds(30));
+
+        }
+        [HttpGet]
+
+        public string Get()
+        {
+            if (!_memoryCache.TryGetValue("key", out string value))
+            {
+                return _memoryCache.Get<string>("key");
+            }
+            return "";
+        }
+    }
+}
